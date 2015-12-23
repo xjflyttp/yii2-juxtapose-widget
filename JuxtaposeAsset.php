a8355604c362aef81dad50fff24542a430ad038a
xjflyttp <xjflyttp@gmail.com>
2015-12-23 15:27:49 +0800

    init

---
+++ JuxtaposeAsset.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace xj\juxtapose;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author xjflyttp <xjflyttp@gmail.com>
+ */
+class JuxtaposeAsset extends AssetBundle
+{
+
+    public $sourcePath = '@bower/juxtapose/build';
+    public $basePath = '@webroot/assets';
+    public $js = ['js/juxtapose.js'];
+    public $css = ['css/juxtapose.css'];
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # yii2-juxtapose-widget
+```
 https://github.com/NUKnightLab/juxtapose
+```
+### composer.json
+---
+```json
+"require": {
+    "xj/yii2-juxtapose-widget": "~1.0"
+},
+```
+
+### Assets
+---
+```php
+xj\juxtapose\JuxtaposeAsset::register($this);
+```
